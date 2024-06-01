4c48df333c408bc9939e6731a6f2dc625d86b053
Rishi Timalsina <rishi.timilsina@nexdigitalstudio.com>
2024-06-01 10:17:54 +0545

    change

--- resources/views/home/partials/testimonial.blade.php
+++ resources/views/home/partials/testimonial.blade.php
@@ -1,5 +1,5 @@
     <!-- Testimonial Area Starts -->
-    <section id="testimonial" class="bg-[url(assets/images/testimonial-bg.jpg')] bg-center bg-cover bg-no-repeat mt-28 px-10 py-24 relative w-full z-1">
+    <section id="testimonial" class="bg-[url(assets/images/green-peas,jpg')] bg-center bg-cover bg-no-repeat mt-28 px-10 py-24 relative w-full z-1">
       <span class="overlay absolute w-full h-full bg-slate-900/70 left-0 top-0 z-n1"></span>
       <div class="m-4 max-w-md mt-5 p-4 text-center text-white mx-auto">
         <h3 class="font-bold font-cursive-merie text-4xl leading-normal capitalize">Customer <span class="text-amber-400 leading-snug">says</span> . . .</h3>
