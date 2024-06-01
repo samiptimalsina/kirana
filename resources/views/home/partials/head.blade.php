5c184df1b5a605131a110b123fd6756bf950b6cb
Rishi Timalsina <rishi.timilsina@nexdigitalstudio.com>
2024-06-01 11:34:46 +0545

    chage

--- resources/views/home/partials/head.blade.php
+++ resources/views/home/partials/head.blade.php
@@ -5,9 +5,9 @@
 <!-- Page Title -->
 <title>Maha spice and food Product</title>
 <!-- Favicon -->
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="apple-touch-icon" sizes="180x180" href="">
+<link rel="icon" type="image/png" sizes="32x32" href=">
+<link rel="icon" type="image/png" sizes="16x16" href="">
 <link rel="manifest" href="/site.webmanifest">
 <!-- CSS libraries -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css" />
