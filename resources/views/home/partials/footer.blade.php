79eea690794d566c4024b62c9032cd8d2147a684
Rishi Timalsina <rishi.timilsina@nexdigitalstudio.com>
2024-06-03 00:33:43 +0545

    map added

--- resources/views/home/partials/footer.blade.php
+++ resources/views/home/partials/footer.blade.php
@@ -1,42 +1,45 @@
  <!-- Footer Area Starts -->
  <footer id="contact" class="footer-area bg-slate-800 text-white font-sans-lato"  style="background-color: rgba(255, 0, 0, 0.82);">
-     <div class="flex flex-wrap footer-widget justify-between p-3 py-14 max-w-[960px] mx-auto section-padding">
-         <div class="basis-[300px] grow shrink p-5 pr-9 single-widget single-widget1">
-             <a href="/"><img class="w-36 h-auto" src="@if(isset($settings)) {{ $settings->getLogo($settings->logo) }} @endif" alt="" /></a>
-             <p class="mt-3 leading-normal">
-                 Which morning fourth great won't is to fly bearing
-                 man. Called unto shall seed, deep, herb set seed
-                 land divide after over first creeping. First
-                 creature set upon stars.
-             </p>
-         </div>
-         <div class="single-widget single-widget2 leading-loose p-5 pr-7">
-             <h5 class="mb-2 font-bold font-cursive-merie text-lg capitalize text-amber-400">contact us</h5>
-             <div class="flex items-start">
-                 <i class="fa fa-map-marker pr-3 pl-1 pt-2.5"></i>
-                 <p>
-                     Mohan Galli Maitidebi-8, <br />
-                      kathmadu Nepal
-                     +9779860309035
-                 </p>
-             </div>
-             <div class="flex">
-                 <i class="fa fa-phone pr-3 pl-1  pt-2.5"></i>
-                 <p>(123) 456 78 90</p>
-             </div>
-             <div class="flex">
-                 <i class="fa fa-envelope-o pr-3 pl-1 pt-2.5"></i>
-                 <p>support@techzillasoft.com</p>
-             </div>
-         </div>
-         <div class="single-widget single-widget3 leading-loose p-5 justify-self-end">
-             <h5 class="mb-2 font-bold font-cursive-merie text-lg capitalize text-amber-400">opening hours</h5>
-             <p>Monday ...................... Closed</p>
-             <p>Tue-Fri .............. 10 am - 12 pm</p>
-             <p>Sat-Sun ............... 8 am - 11 pm</p>
-             <p>Holidays ............. 10 am - 12 pm</p>
-         </div>
-     </div>
+        <div class="flex flex-wrap footer-widget justify-center p-3 py-14 max-w-[1080px] mx-auto section-padding gap-5">
+            <div class="basis-full md:basis-[300px] flex-grow p-5 single-widget single-widget1">
+                <a href="/"><img class="w-36 h-auto" src="@if(isset($settings)) {{ $settings->getLogo($settings->logo) }} @endif" alt="" /></a>
+                <p class="mt-3 leading-normal">
+                    Which morning fourth great won't is to fly bearing
+                    man. Called unto shall seed, deep, herb set seed
+                    <br>
+                    land divide after over first creeping. First
+                    creature set upon stars.
+                </p>
+            </div>
+            <div class="basis-full md:basis-[300px] flex-grow single-widget single-widget2 leading-loose p-5">
+                <h5 class="mb-2 font-bold font-cursive-merie text-lg capitalize text-amber-400">contact us</h5>
+                <div class="flex items-start">
+                    <i class="fa fa-map-marker pr-3 pl-1 pt-2.5"></i>
+                    <p>
+                        Mohan Galli Maitidebi-8, <br />
+                        Kathmandu Nepal
+                    </p>
+                </div>
+                <div class="flex">
+                    <i class="fa fa-phone pr-3 pl-1 pt-2.5"></i>
+                    <p>                        +9779860309035
+</p>
+                </div>
+                <div class="flex">
+                    <i class="fa fa-envelope-o pr-3 pl-1 pt-2.5"></i>
+                    <p>support@techzillasoft.com</p>
+                </div>
+            </div>
+            <div class="basis-full md:basis-[300px] flex-grow single-widget single-widget3 leading-loose p-5">
+                <div style="width: 100%; height: 200px; overflow: hidden;">
+            <iframe width="100%" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=200&amp;hl=en&amp;q=Kathmandu,%20Nepal&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+                </div>
+            </div>
+        </div>
+
+
+
+
      <div class="footer-copyright py-3 text-white bg-slate-900" style="background-color: rgba(246,224,1,1); color:#000;">
          <div class="flex flex-wrap items-center max-w-[960px] mx-auto px-3 justify-between">
              <span class="my-3 mx-5 leading-normal">
@@ -50,7 +53,7 @@
              </span>
                 <ul class="social-icons flex flex-wrap my-3 mx-5 transition ease-in-out duration-300">
                     <li class="mr-4 my-1">Follow Us:</li>
-                    <li class="mr-3 my-1 cursor-pointer border rounded-full border-transparent hover:border-white hover:rounded-full  px-1 bg-blue-500"
+                    <li class="mr-3 my-1 cursor-pointer border rounded-full border-transparent hover:border-white hover:rounded-full m- px-1 bg-blue-500"
                         style="background-color: #3b5998;">
                         <a href="#" class="cursor-pointer text-white"><i class="fa-brands fa-facebook"></i></a>
                     </li>
