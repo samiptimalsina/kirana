0038661440953673efca59f143530bbce134c368
Rishi Timalsina <rishi.timilsina@nexdigitalstudio.com>
2024-06-02 23:06:02 +0545

    change

--- resources/views/home/partials/footer.blade.php
+++ resources/views/home/partials/footer.blade.php
@@ -1,5 +1,5 @@
  <!-- Footer Area Starts -->
- <footer id="contact" class="footer-area bg-slate-800 text-white font-sans-lato">
+ <footer id="contact" class="footer-area bg-slate-800 text-white font-sans-lato"  style="background-color: rgba(255, 0, 0, 0.82);">
      <div class="flex flex-wrap footer-widget justify-between p-3 py-14 max-w-[960px] mx-auto section-padding">
          <div class="basis-[300px] grow shrink p-5 pr-9 single-widget single-widget1">
              <a href="/"><img class="w-36 h-auto" src="@if(isset($settings)) {{ $settings->getLogo($settings->logo) }} @endif" alt="" /></a>
@@ -37,7 +37,7 @@
              <p>Holidays ............. 10 am - 12 pm</p>
          </div>
      </div>
-     <div class="footer-copyright py-3 text-white bg-slate-900">
+     <div class="footer-copyright py-3 text-white bg-slate-900" style="background-color: rgba(246,224,1,1); color:#000;">
          <div class="flex flex-wrap items-center max-w-[960px] mx-auto px-3 justify-between">
              <span class="my-3 mx-5 leading-normal">
                  Copyright &copy;
@@ -48,24 +48,32 @@
                  inspired by
                  <a class="text-cyan-400" href="https://techzillasoft.com" target="_blank">TechzillaSoft</a>
              </span>
-             <ul class="social-icons flex flex-wrap my-3 mx-5 transition ease-in-out duration-300">
-                 <li class="mr-4 my-1">Follow Us:</li>
-                 <li class="mr-3 my-1 cursor-pointer border border-transparent hover:border-white hover:rounded-[50%] px-1">
-                     <a href="#" class="cursor-pointer"><i class="fa-brands fa-facebook"></i></a>
-                 </li>
-                 <li class="mr-3 my-1 cursor-pointer border border-transparent hover:border-white hover:rounded-[50%] px-1">
-                     <a href="#" class="cursor-pointer"><i class="fa fa-twitter"></i></a>
-                 </li>
-                 <li class="mr-3 my-1 cursor-pointer border border-transparent hover:border-white hover:rounded-[50%] px-1">
-                     <a href="#" class="cursor-pointer"><i class="fa-brands fa-google-plus"></i></a>
-                 </li>
-                 <li class="mr-3 my-1 cursor-pointer border border-transparent hover:border-white hover:rounded-[50%] px-1">
-                     <a href="#" class="cursor-pointer"><i class="fa-brands fa-pinterest"></i></a>
-                 </li>
-                 <li class="my-1 cursor-pointer border border-transparent hover:border-white hover:rounded-[50%] px-1">
-                     <a href="#" class="cursor-pointer"><i class="fa fa-instagram"></i></a>
-                 </li>
-             </ul>
+                <ul class="social-icons flex flex-wrap my-3 mx-5 transition ease-in-out duration-300">
+                    <li class="mr-4 my-1">Follow Us:</li>
+                    <li class="mr-3 my-1 cursor-pointer border rounded-full border-transparent hover:border-white hover:rounded-full  px-1 bg-blue-500"
+                        style="background-color: #3b5998;">
+                        <a href="#" class="cursor-pointer text-white"><i class="fa-brands fa-facebook"></i></a>
+                    </li>
+                    <li class="mr-3 my-1 cursor-pointer border rounded-full border-transparent hover:border-white hover:rounded-full px-1 bg-twitter"
+                        style="background-color: #1DA1F2;">
+                        <a href="#" class="cursor-pointer text-white"><i class="fa-brands fa-twitter"></i></a>
+                    </li>
+                    <li class="mr-3 my-1 cursor-pointer border rounded-full border-transparent hover:border-white hover:rounded-full px-1 bg-red-600"
+                        style="background-color: #DB4437;">
+                        <a href="#" class="cursor-pointer rounded-full text-white"><i class="fa-brands fa-google-plus"></i></a>
+                    </li>
+                    <li class="mr-3 my-1 cursor-pointer border rounded-full border-transparent hover:border-white hover:rounded-full px-1 bg-pinterest"
+                        style="background-color: #E60023;">
+                        <a href="#" class="cursor-pointer text-white"><i class="fa-brands fa-pinterest"></i></a>
+                    </li>
+                    <li class="my-1 cursor-pointer border rounded-full border-transparent hover:border-white hover:rounded-full px-1 bg-instagram"
+                        style="background-color: #C13584;">
+                        <a href="#" class="cursor-pointer text-white"><i class="fa-brands fa-instagram"></i></a>
+                    </li>
+                </ul>
+
+
+
          </div>
      </div>
  </footer>
