330fd7aec50eafebd09ce5e9a053486bbbe39180
kingbhishma9 <kingbhishma9@gmail.com>
2024-06-20 23:48:13 -0400

    cdscsdc

---
+++ storage/framework/views/6ce135bfb3b775dc835814a7ca8211c6e1cfd2ca.php
@@ -0,0 +1,91 @@
+<?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
+<?php $component = $__env->getContainer()->make(Illuminate\View\AnonymousComponent::class, ['view' => 'components.admin.index','data' => ['user' => $user,'isAdmin' => $isAdmin]]); ?>
+<?php $component->withName('admin.index'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php $component->withAttributes(['user' => \Illuminate\View\Compilers\BladeCompiler::sanitizeComponentAttribute($user),'isAdmin' => \Illuminate\View\Compilers\BladeCompiler::sanitizeComponentAttribute($isAdmin)]); ?>
+	<div class="content-wrapper">
+		<a href="<?php echo e(route('specialdishes.create')); ?>" class="btn btn-primary mx-2">Add Special Product</a>
+	</div>
+
+	<div class="content-wrapper">
+		<div class="col-lg-12 grid-margin stretch-card">
+			<div class="card">
+				<div class="card-body">
+					<h4 class="card-title">Special-Dish Data-Table</h4>
+					<p class="card-description">
+						Special-Dish information table
+					</p>
+
+					<?php if(session()->has('msg')): ?>
+					<p class="alert alert-info"><?php echo e(session()->get('msg')); ?></p>
+					<?php endif; ?>
+
+					<table class="table table-hover overflow-auto block">
+						<thead>
+							<tr class="bg-slate-800">
+								<?php $__currentLoopData = ["Image", "Name", "Price", "Description", "Created at", "", ""]; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $heading): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+									<th class="font-bold text-white"><?php echo e($heading); ?></th>
+								<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+							</tr>
+						</thead>
+						<tbody>
+							<?php $__currentLoopData = $data; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $data): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+							<tr>
+								<td class="w-32">
+									<img src="<?php echo e($data->img); ?>" alt="<?php echo e($data->name); ?>" class="!w-full !h-auto !rounded-none">
+								</td>
+								<td>
+									<p class="font-bold">
+		                <span class="text-amber-400"><?php echo e($data['namepart1']); ?></span> <span class="leading-normal"><?php echo e($data['namepart2']); ?></span>
+		              </p>
+		            </td>
+								<td><?php echo e($data->price); ?></td>
+								<td class="max-w-[190px] min-w-[190px] !leading-normal !whitespace-normal break-words"><?php echo e($data->desc); ?></td>
+								<td><?php echo e($data->created_at); ?></td>
+								<td>
+									<a
+										href="<?php echo e(route('specialdishes.edit', $data->id)); ?>"
+										class="badge badge-primary cursor-pointer"
+										>Edit</a
+									>
+								</td>
+								<td>
+									<?php if($isAdmin === true): ?>
+									<form method="POST" action="<?php echo e(route('specialdishes.destroy', $data->id)); ?>">
+						        <?php echo method_field('DELETE'); ?>
+										<?php echo csrf_field(); ?>
+					        	<button
+					        		type="submit"
+					        		class="badge badge-danger cursor-pointer"
+					        		onclick="return confirmDeleteSpDish(<?php echo e($data->id); ?> , '<?php echo e($data->name); ?>');"
+					        		>Delete</button>
+								  </form>
+									<?php else: ?>
+									<button
+										onclick="alert('Only admin can special dish info')"
+										class="badge badge-danger cursor-pointer"
+										>Delete</button>
+									<?php endif; ?>
+								</td>
+							</tr>
+							<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script>
+  function confirmDeleteSpDish(id, name) {
+      if(!confirm("Are You Sure to delete this special dish item, Named: " + name + ", Id: " + id + "." ))
+      event.preventDefault();
+  }
+ </script>
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
+<?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
+<?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
+<?php endif; ?>
+<?php /**PATH /home/samiptimalsina/betal.techzillasoft.com/resources/views/admin/pages/spdishes/spdishes.blade.php ENDPATH**/ ?>
