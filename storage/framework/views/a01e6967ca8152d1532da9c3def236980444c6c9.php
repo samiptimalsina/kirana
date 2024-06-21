59c3f97353e85101633290c6d3f91c996b0ee168
kingbhishma9 <kingbhishma9@gmail.com>
2024-06-20 23:57:27 -0400

    blog

--- storage/framework/views/a01e6967ca8152d1532da9c3def236980444c6c9.php
+++ storage/framework/views/a01e6967ca8152d1532da9c3def236980444c6c9.php
@@ -1,7 +1,7 @@
   <!-- Banner Area Starts -->
   <section id="home"
       class="w-full h-screen flex flex-col justify-center items-center bg-cover bg-center bg-no-repeat"
-      style="background-image: url(<?php echo e($bannerImg); ?>)">
+      style="background-image: url(<?php echo e($banner_image->image_url); ?>)">
       
       <h1 class="font-cursive-merie text-white text-center leading-normal text-6xl">
           <span class="text-amber-400"><?php echo e($banner->title ?? ''); ?></span><br />
