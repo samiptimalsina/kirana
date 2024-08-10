9ae189c070a6626d72e1cd3250e65243c695e1ad
Rishi Timalsina <rishi.timilsina@nexdigitalstudio.com>
2024-08-10 19:51:31 +0545

    ckjsdfnadsf

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,13 +2,10 @@
 
 namespace App\Providers;
 
-use App\Models\Setting;
 use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\View;
-use Illuminate\Database\QueryException;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,17 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-
-        try {
-            $setting = Setting::first(); // Adjust this if needed
-            $logo = $setting ? $setting->getLogoPath('logo.png') : null;
-        } catch (ModelNotFoundException $e) {
-            $logo = null;
-        } catch (QueryException $e) {
-            $logo = null;
-        } catch (\Exception $e) {
-            $logo = null;
-        }
         $user = Auth::id() ? Auth::user() : null;
         $isAdmin = $this->GetIsAdmin();
         View::composer('*', function ($view) use ($user, $isAdmin) {
