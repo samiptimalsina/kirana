2c929358bb84c2a6f67aa838cf3145c34f14686e
Rishi Timalsina <rishi.timilsina@nexdigitalstudio.com>
2024-08-10 14:42:02 +0545

    cha nge

--- database/seeders/FoodSeeder.php
+++ database/seeders/FoodSeeder.php
@@ -14,6 +14,8 @@ class FoodSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
         DB::table('food')->truncate();
         $products = [
 
