2e3df043478901c0eb33062f3046506f97c49236
Rishi Timalsina <rishi.timilsina@nexdigitalstudio.com>
2024-08-12 02:35:41 +0545

    change

--- resources/views/home/shop.blade.php
+++ resources/views/home/shop.blade.php
@@ -23,5 +23,23 @@
 
     @include('home.partials.script')
 </body>
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    // Get the last scroll position
+    let lastScrollPosition = parseInt(sessionStorage.getItem('scrollPosition') || '0', 10);
 
+    // Ensure the scroll position is within 600px from the top
+    if (lastScrollPosition > 1000) {
+      lastScrollPosition = 1000;
+    }
+
+    // Restore scroll position
+    window.scrollTo(0, lastScrollPosition);
+
+    // Save scroll position before unloading
+    window.addEventListener('beforeunload', function() {
+      sessionStorage.setItem('scrollPosition', window.scrollY);
+    });
+  });
+</script>
 </html>
