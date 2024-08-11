7bf8ac1f294c68c689a8051abfc0d5b2b7cb44ef
Rishi Timalsina <rishi.timilsina@nexdigitalstudio.com>
2024-08-11 22:41:35 +0545

    change

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -126,7 +126,7 @@ class HomeController extends Controller
 
     public function blogs()
     {
-        $blogs = Blog::latest()->paginate(51);
+        $blogs = Blog::latest()->paginate(11);
 
         return view('home.blogs', [
             'navdata' => $this->navdata,
