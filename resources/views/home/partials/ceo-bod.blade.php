205109183cd01a55721b2b089ded61811d1cf72f
Rishi Timalsina <rishi.timilsina@nexdigitalstudio.com>
2024-08-11 23:11:07 +0545

    change

---
+++ resources/views/home/partials/ceo-bod.blade.php
@@ -0,0 +1,43 @@
+<section id="ceo-message">
+    <div class="ceo-container">
+        <div class="ceo-content">
+            <h3>Welcome to Maha Spice and Food Products Industries Pvt. Ltd.</h3>
+            <p>
+               I am Madan Raj Neupane, the founder and Managing Director. Our journey began with a simple dream and a passion for bringing quality FMCG products to your doorstep. From selling water and retail items to establishing our manufacturing unit, our story is one of perseverance, dedication, and growth.
+               <br/>
+
+At Maha Spice, we are committed to producing the finest products such as Kurkure, Cheeseballs, Lays Bhuja, Dalmote, and Namkeen. Despite facing challenges, we have remained steadfast in our mission to deliver excellence. Our transition from a proprietorship to a private limited company is a testament to our commitment to growth and innovation.
+  <br/>
+We believe in inspiring our community and contributing to our country's progress. With deep faith in the Bhagwat Gita, we find strength to overcome challenges and strive for success.
+  <br/>
+Thank you for being a part of our journey. We look forward to serving you with the best products and services.
+            </p>
+            <p class="ceo-name">Madan Raj Neupane</p>
+            <p class="ceo-position">Chief Executive Officer</p>
+        </div>
+        <div class="ceo-image">
+            <img src="{{  $settings->about_us_image($settings->about_us_image) ?? ''}}" alt="CEO Image" />
+        </div>
+    </div>
+</section>
+
+
+<section id="board-of-directors" class="max-w-screen-xl mx-auto p-8">
+    <div class="flex flex-wrap items-center justify-center gap-8">
+        <div class="flex-1 max-w-md">
+            <img class="w-full h-auto rounded-lg shadow-lg" src="{{  $settings->about_us_image($settings->about_us_image) ?? ''}}" alt="Board of Directors Image" />
+        </div>
+        <div class="flex-1 text-gray-600">
+            <h3 class="text-2xl font-cursive mb-4 text-gray-900">Meet Our Board of Directors</h3>
+            <p class="text-base leading-relaxed mb-4">
+
+Mr. Sushant Neupane,being a board of director in a company responsible for overseeing the management and direction of a company as well as  responsible for selecting and appointing the executive leadership, including the CEO.Acts  as  the governing body of a company.
+               <br/>
+Mr. Neupane is responsible for overseeing the activities, policies, and strategic direction of a company and represents the interests of shareholders or members ensuring that the company is managed in a way that aligns with its mission, goals, and legal obligations. He as a director plays a crucial role in the leadership and governance of a company, ensuring it operates effectively, ethically, and in alignment with its stated goals and legal requirements.               <br/>
+However, as a company’s member, his main focus is to manufacture best quality of products, creating new taste for our valued customers and is to build a company that can achieve the highest levels of customer satisfaction with highest levels of integrity and business ethics.
+            </p>
+            <p class="text-xl font-bold text-gray-900">Sushant Neupane</p>
+            <p class="text-base text-lime-500">Chairman of the Board</p>
+        </div>
+    </div>
+</section>
---
+++ resources/views/home/partials/vision.blade.php
@@ -0,0 +1,21 @@
+<section id="mission-vision" class="max-w-screen-xl mx-auto p-8">
+    <div class="text-gray-700">
+        <!-- Mission Section -->
+        <div class="mb-12">
+           <p class="text-xl font-bold text-gray-900">Our Mission</p>
+            <p class="text-lg leading-relaxed mb-6">
+We hope  to provide best quality food products to our customers and  aim to become leading brand of snacks and implement the best in technology, research and development.<br/>
+We sustain growth trajectory by  attracting  and attaining customer loyalty.
+            </p>
+        </div>
+
+        <!-- Vision Section -->
+        <div>
+           <p class="text-xl font-bold text-gray-900">Our Vision</p>
+            <p class="text-lg leading-relaxed mb-6">
+Our vision is  to manufacture best quality of products, creating new taste and recipes for our valued customers ,there by maintaing consumer loyalty . <br/>
+ we aim to become the National distributor in the aspect of quality and healthy food products as well as famous  brand of the nation.
+            </p>
+        </div>
+    </div>
+</section>
