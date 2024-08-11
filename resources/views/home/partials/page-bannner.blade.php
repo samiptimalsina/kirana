5247ab0ba33cf7e2101546833ff7517bca608c13
Rishi Timalsina <rishi.timilsina@nexdigitalstudio.com>
2024-08-11 19:45:25 +0545

    change

--- resources/views/home/partials/page-bannner.blade.php
+++ resources/views/home/partials/page-bannner.blade.php
@@ -1,7 +1,7 @@
 <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
 
-<section id="bannerSection" class="relative w-full h-64 md:h-80 lg:h-96 overflow-hidden">
-    <img id="bannerImage" src="{{$banner_image->image_url}}" alt="Banner Image" class="w-full h-full object-cover max-w-full">
+<section id="bannerSection" class="relative w-full overflow-hidden" style="height: 600px;">
+    <img id="bannerImage" src="{{$banner_image->image_url}}" alt="Banner Image" class="w-full h-full object-cover">
     <div class="absolute inset-0 flex items-center justify-center text-center text-white bg-black bg-opacity-50">
         <h1 class="text-2xl md:text-3xl lg:text-4xl font-bold">{{ $title ?? '' }}</h1>
     </div>
