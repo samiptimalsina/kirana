197c0b447d5b2e985a8c6c505d751f6b87c5f2b6
Rishi Timalsina <rishi.timilsina@nexdigitalstudio.com>
2024-08-11 22:39:25 +0545

    change

--- resources/views/home/about-us.blade.php
+++ resources/views/home/about-us.blade.php
@@ -6,21 +6,23 @@
 </head>
 
 <body>
-    					@if(session()->has('msg'))
-					<p class="alert alert-info">{{ session()->get('msg') }}</p>
-					@endif
+    @if(session()->has('msg'))
+        <p class="alert alert-info">{{ session()->get('msg') }}</p>
+    @endif
+
     @include('home.partials.preloader')
     @include('home.partials.header', ['navdata' => $navdata])
-    @include('home.partials.page-bannner', ['title' => 'Maha spice and food Product
-'])
+    @include('home.partials.page-bannner', ['title' => 'Maha Spice and Food Product'])
 
     @include('home.partials.welcome')
+
+    @include('home.partials.mission') <!-- Our Mission Section -->
+    @include('home.partials.vision')  <!-- Our Vision Section -->
 
     @include('home.partials.teams', [
         'testimonialdata' => $testimonialdata,
         'quoteImg' => 'assets/images/quote.png',
     ])
-
 
     {{-- @include('home.partials.table', ['tableBg' => 'https://betal.techzillasoft.com/assets/images/banner/5M6qu_1718908856.jpeg']) --}}
     @include('home.partials.footer')
