bd927b95fdc0532565c8097c4a52d8b72714e5a6
Rishi Timalsina <rishi.timilsina@nexdigitalstudio.com>
2024-08-12 03:14:54 +0545

    change

--- resources/views/home/partials/page-bannner.blade.php
+++ resources/views/home/partials/page-bannner.blade.php
@@ -26,7 +26,7 @@
 
 <section id="bannerSection">
     <!-- Image tag -->
-    <img id="bannerImage" class="banner-image" src="{{ $banner_image->image_url ?? '' }}" alt="Banner Image" style="height: 400px;">
+    <img id="bannerImage" class="banner-image" src="{{ $banner_image->image_url ?? '' }}" alt="Banner Image" style="height: 300px;">
 
     <div class="absolute inset-0 bg-black opacity-40"></div>
 
