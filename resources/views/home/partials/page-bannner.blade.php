05f65bd9bc29b667d38a8a41771db6de9de42a85
Rishi Timalsina <rishi.timilsina@nexdigitalstudio.com>
2024-08-12 03:13:32 +0545

    ch

--- resources/views/home/partials/page-bannner.blade.php
+++ resources/views/home/partials/page-bannner.blade.php
@@ -22,7 +22,19 @@
         text-align: center;
     }
 </style>
+@if(Route::currentRouteName() === 'shop')
 
+<section id="bannerSection">
+    <!-- Image tag -->
+    <img id="bannerImage" class="banner-image" src="{{ $banner_image->image_url ?? '' }}" alt="Banner Image" style="height: 400px;">
+
+    <div class="absolute inset-0 bg-black opacity-40"></div>
+
+    <div class="banner-content text-white">
+        <h1 class="text-4xl font-bold">{{ $title ?? '' }}</h1>
+    </div>
+</section>
+@else
 <section id="bannerSection">
     <!-- Image tag -->
     <img id="bannerImage" class="banner-image" src="{{ $banner_image->image_url ?? '' }}" alt="Banner Image">
@@ -33,3 +45,4 @@
         <h1 class="text-4xl font-bold">{{ $title ?? '' }}</h1>
     </div>
 </section>
+@endif
