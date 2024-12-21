621482742abd15ae466d08bcf5c9b1ec5b170a19
Rishi Timalsina <rishi.timilsina@nexdigitalstudio.com>
2024-12-21 09:23:44 +0545

    change on resoer

--- resources/views/home/blog-detail.blade.php
+++ resources/views/home/blog-detail.blade.php
@@ -72,8 +72,8 @@
     <div class=" mx-auto bg-white shadow-md rounded-lg p-6">
         {{-- <p class="text-gray-600 mb-4">Published on <span class="font-semibold">June 1, 2024</span> by <span
                 class="font-semibold">John Doe</span></p> --}}
-        <img src="{{ $blog->image }}" alt="{{ $blog->title ?? '' }}" alt="Blog Image"
-            class="w-full h-auto rounded-lg mb-4">
+        <img src="{{ url($blog->image) }}" alt="{{ $blog->title ?? '' }}" alt="Blog Image"
+            class="w-full h-auto rounded-lg mb-4" style="height: 550px;">
         <br>
 
         <h2 class="text-xl font-semibold text-gray-800 mb-2 mt-2">{{ $blog->title }}</h2>
