ece077244fe6a8571863810572eab2dba094929e
Rishi Timalsina <rishi.timilsina@nexdigitalstudio.com>
2024-12-21 09:10:03 +0545

    change

--- resources/views/home/blog-detail.blade.php
+++ resources/views/home/blog-detail.blade.php
@@ -60,32 +60,34 @@
     </style>
 </head>
 
-    @if(session()->has('msg'))
-					<p class="alert alert-info">{{ session()->get('msg') }}</p>
-					@endif
-    @include('home.partials.preloader')
-    @include('home.partials.header', ['navdata' => $navdata])
-    @include('home.partials.page-bannner', ['title' => "Blog Details"])
+@if (session()->has('msg'))
+    <p class="alert alert-info">{{ session()->get('msg') }}</p>
+@endif
+@include('home.partials.preloader')
+@include('home.partials.header', ['navdata' => $navdata])
+@include('home.partials.page-bannner', ['title' => 'Blog Details'])
 
 
-    <main class="container mx-auto py-6">
-        <div class=" mx-auto bg-white shadow-md rounded-lg p-6">
-            <p class="text-gray-600 mb-4">Published on <span class="font-semibold">June 1, 2024</span> by <span class="font-semibold">John Doe</span></p>
-        <img src="{{ asset('assets/images/' . $blog->image) }}" alt="Blog Image" class="w-full h-auto rounded-lg mb-4">
-                        <br>
+<main class="container mx-auto py-6">
+    <div class=" mx-auto bg-white shadow-md rounded-lg p-6">
+        {{-- <p class="text-gray-600 mb-4">Published on <span class="font-semibold">June 1, 2024</span> by <span
+                class="font-semibold">John Doe</span></p> --}}
+        <img src="{{ $blog->image }}" alt="{{ $blog->title ?? '' }}" alt="Blog Image"
+            class="w-full h-auto rounded-lg mb-4">
+        <br>
 
-                <h2 class="text-xl font-semibold text-gray-800 mb-2 mt-2">{{ $blog->title }}</h2>
-                <br>
-            <p class="text-gray-800 leading-relaxed mb-4">{{$blog->short_desc ?? ''}}</p>
-            <p class="text-gray-800 leading-relaxed mb-4">{{$blog->content ?? ''}}</p>
-        </div>
-    </main>
+        <h2 class="text-xl font-semibold text-gray-800 mb-2 mt-2">{{ $blog->title }}</h2>
+        <br>
+        <p class="text-gray-800 leading-relaxed mb-4">{{ $blog->short_desc ?? '' }}</p>
+        <p class="text-gray-800 leading-relaxed mb-4">{{ $blog->content ?? '' }}</p>
+    </div>
+</main>
 
 
-        {{-- @include('home.partials.table', ['tableBg' => 'assets/images/table-bg.jpg']) --}}
-    @include('home.partials.footer')
+{{-- @include('home.partials.table', ['tableBg' => 'assets/images/table-bg.jpg']) --}}
+@include('home.partials.footer')
 
-    @include('home.partials.script')
+@include('home.partials.script')
 </body>
 
 </html>
