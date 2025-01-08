bba74a91b3518de0527e858ce556a52f93a70095
Rishi Timalsina <rishi.timilsina@nexdigitalstudio.com>
2025-01-08 22:51:23 +0545

    change

--- resources/views/home/partials/ceo-bod.blade.php
+++ resources/views/home/partials/ceo-bod.blade.php
@@ -1,92 +1,43 @@
 <section id="ceo-message" class="max-w-screen-xl mx-auto p-8">
     <div class="flex flex-wrap items-center justify-between gap-8">
         <div class="w-full lg:w-1/2 text-gray-600">
-            <h3 class="text-2xl font-cursive mb-4 text-gray-900">Welcome to Maha Spice and Food Products Industries Pvt. Ltd.</h3>
+            <h3 class="text-2xl font-cursive mb-4 text-gray-900">Welcome to Maha Spice and Food Products Industries Pvt.
+                Ltd.</h3>
             <p class="text-base leading-relaxed mb-4">
-                I am Madan Raj Neupane, the founder and Managing Director. Our journey began with a simple dream and a passion for bringing quality FMCG products to your doorstep. From selling water and retail items to establishing our manufacturing unit, our story is one of perseverance, dedication, and growth.
-                <br/>
-                At Maha Spice, we are committed to producing the finest products such as Kurkure, Cheeseballs, Lays Bhuja, Dalmote, and Namkeen. Despite facing challenges, we have remained steadfast in our mission to deliver excellence. Our transition from a proprietorship to a private limited company is a testament to our commitment to growth and innovation.
-                <br/>
-                We believe in inspiring our community and contributing to our country's progress. With deep faith in the Bhagwat Gita, we find strength to overcome challenges and strive for success.
-                <br/>
-                Thank you for being a part of our journey. We look forward to serving you with the best products and services.
+                I am Madan Raj Neupane, the founder and Managing Director. Our journey began with a simple dream and a
+                passion for bringing quality FMCG products to your doorstep. From selling water and retail items to
+                establishing our manufacturing unit, our story is one of perseverance, dedication, and growth.
+                <br />
+                At Maha Spice, we are committed to producing the finest products such as Kurkure, Cheeseballs, Lays
+                Bhuja, Dalmote, and Namkeen. Despite facing challenges, we have remained steadfast in our mission to
+                deliver excellence. Our transition from a proprietorship to a private limited company is a testament to
+                our commitment to growth and innovation.
+                <br />
+                We believe in inspiring our community and contributing to our country's progress. With deep faith in the
+                Bhagwat Gita, we find strength to overcome challenges and strive for success.
+                <br />
+                Thank you for being a part of our journey. We look forward to serving you with the best products and
+                services.
             </p>
             <p class="text-xl font-bold text-gray-900">Madan Raj Neupane</p>
             <p class="text-base text-lime-500">Chief Executive Officer</p>
         </div>
         <div class="w-full lg:w-1/2 max-w-md">
-            <img class="w-full h-auto rounded-lg shadow-lg" src="{{url('/')}}/teams/photo/coe.jpeg" alt="CEO Image" />
+            <img class="w-full h-auto rounded-lg shadow-lg" src="{{ url('/') }}/teams/photo/sushant_neupane.jpeg"
+                alt="CEO Image" />
         </div>
     </div>
 </section>
-
-{{-- <section id="board-of-directors" class="max-w-screen-xl mx-auto p-8">
-    <div class="flex flex-wrap items-center justify-between gap-8">
-        <div class="w-full lg:w-1/2 max-w-md">
-            <img class="w-full h-auto rounded-lg shadow-lg" src="{{url('/')}}/teams/photo/1723140846.jpg" alt="Board of Directors Image" />
-        </div>
-        <div class="w-full lg:w-1/2 text-gray-600">
-            <h3 class="text-2xl font-cursive mb-4 text-gray-900">Meet Our Board of Directors</h3>
-            <p class="text-base leading-relaxed mb-4">
-                Mr. Sushant Neupane, being a board of director in a company responsible for overseeing the management and direction of a company as well as responsible for selecting and appointing the executive leadership, including the CEO. Acts as the governing body of a company.
-                <br/>
-                Mr. Neupane is responsible for overseeing the activities, policies, and strategic direction of a company and represents the interests of shareholders or members ensuring that the company is managed in a way that aligns with its mission, goals, and legal obligations. He as a director plays a crucial role in the leadership and governance of a company, ensuring it operates effectively, ethically, and in alignment with its stated goals and legal requirements.
-                <br/>
-                However, as a company’s member, his main focus is to manufacture best quality of products, creating new taste for our valued customers and is to build a company that can achieve the highest levels of customer satisfaction with highest levels of integrity and business ethics.
-            </p>
-            <p class="text-xl font-bold text-gray-900">Sushant Neupane</p>
-            <p class="text-base text-lime-500">BOD</p>
-        </div>
-    </div>
-</section> --}}
-
-
-
-
-
-{{-- <section id="board-of-directors" class="max-w-screen-xl mx-auto p-8">
-    <div class="flex flex-wrap items-center justify-between gap-8">
-        <div class="w-full lg:w-1/2 max-w-md">
-            <img class="w-full h-auto rounded-lg shadow-lg" src="{{ url('/') }}/teams/photo/sushant_neupane.jpeg"
-                alt="Board of Directors Image" />
-        </div>
-        <div class="w-full lg:w-1/2 text-gray-600">
-            <h3 class="text-2xl font-cursive mb-4 text-gray-900">Meet Our Board of Directors</h3>
-            <p class="text-base leading-relaxed mb-4">
-                Mr. Sushant Neupane, being a board of director in a company responsible for overseeing the management
-                and direction of a company as well as responsible for selecting and appointing the executive leadership
-                and acts as the governing body of a company. <br />
-                <br />
-                Mr. Neupane is responsible for overseeing the activities, policies, and strategic direction of a company
-                and represents the interests of shareholders or members ensuring that the company is managed in a way
-                that aligns with its mission, goals, and legal obligations. He as a director plays a crucial role in the
-                leadership and governance of a company, ensuring it operates effectively, ethically, and in alignment
-                with its stated goals and legal requirements.
-                However, as a company’s member, his main focus is to manufacture best quality of products, creating new
-                taste for our valued customers and is to build a company that can achieve the highest levels of customer
-                satisfaction with highest levels of integrity and business ethics.
-            </p>
-            <p class="text-xl font-bold text-gray-900">Sushant Neupane</p>
-            <p class="text-base text-lime-500">BOD</p>
-        </div>
-    </div>
-</section> --}}
-
-
 
 <section id="board-of-directors" class="max-w-screen-xl mx-auto p-8">
     <div class="flex flex-wrap items-center justify-between gap-8">
         <div class="w-full lg:w-1/2 max-w-md">
-            <img class="w-full h-auto rounded-lg shadow-lg" src="{{ url('/') }}/teams/photo/sushant_neupane.jpeg"
+            <img class="w-full h-auto rounded-lg shadow-lg" src="{{ url('/') }}/teams/photo/1723140846.jpg"
                 alt="Board of Directors Image" />
         </div>
         <div class="w-full lg:w-1/2 text-gray-600">
             <h3 class="text-2xl font-cursive mb-4 text-gray-900">Meet Our Board of Directors</h3>
             <p class="text-base leading-relaxed mb-4">
-                Mr. Sushant Neupane, being a board of director in a company responsible for overseeing the management
-                and direction of a company as well as responsible for selecting and appointing the executive leadership
-                and acts as the governing body of a company. <br />
-                <br />
                 Mr. Neupane is responsible for overseeing the activities, policies, and strategic direction of a company
                 and represents the interests of shareholders or members ensuring that the company is managed in a way
                 that aligns with its mission, goals, and legal obligations. He as a director plays a crucial role in the
@@ -94,39 +45,10 @@
                 with its stated goals and legal requirements.
                 However, as a company’s member, his main focus is to manufacture best quality of products, creating new
                 taste for our valued customers and is to build a company that can achieve the highest levels of customer
-                satisfaction with highest levels of integrity and business ethics.
+                satisfaction with highest levels of integrity and business ethics. <br />
             </p>
             <p class="text-xl font-bold text-gray-900">Sushant Neupane</p>
             <p class="text-base text-lime-500">BOD</p>
         </div>
     </div>
 </section>
-
-
-
-{{-- <section id="ceo-message" class="max-w-screen-xl mx-auto p-8">
-    <div class="flex flex-wrap items-center justify-between gap-8">
-        <div class="w-full lg:w-1/2 text-gray-600">
-            <h3 class="text-2xl font-cursive mb-4 text-gray-900">Meet Our Board of Directors</h3>
-            <p class="text-base leading-relaxed mb-4">
-                Mr. Sushant Neupane, being a board of director in a company responsible for overseeing the management
-                and direction of a company as well as responsible for selecting and appointing the executive leadership
-                and acts as the governing body of a company. <br />
-                <br />
-                Mr. Neupane is responsible for overseeing the activities, policies, and strategic direction of a company
-                and represents the interests of shareholders or members ensuring that the company is managed in a way
-                that aligns with its mission, goals, and legal obligations. He as a director plays a crucial role in the
-                leadership and governance of a company, ensuring it operates effectively, ethically, and in alignment
-                with its stated goals and legal requirements.
-                However, as a company’s member, his main focus is to manufacture best quality of products, creating new
-                taste for our valued customers and is to build a company that can achieve the highest levels of customer
-                satisfaction with highest levels of integrity and business ethics.
-            </p>
-            <p class="text-xl font-bold text-gray-900">Sushant Neupane</p>
-            <p class="text-base text-lime-500">BOD</p>
-        </div>
-        <div class="w-full lg:w-1/2 max-w-md">
-            <img class="w-full h-auto rounded-lg shadow-lg" src="{{ url('/') }}/teams/photo/sushant_neupane.jpeg"
-                alt="Board of Directors Image" />        </div>
-    </div>
-</section> --}}
