8475b6f4ea50b0b7907968906aebdd81d1f4cd52
Rishi Timalsina <rishi.timilsina@nexdigitalstudio.com>
2025-01-08 22:55:01 +0545

    change on releae

--- resources/views/home/partials/ceo-bod.blade.php
+++ resources/views/home/partials/ceo-bod.blade.php
@@ -38,6 +38,10 @@
         <div class="w-full lg:w-1/2 text-gray-600">
             <h3 class="text-2xl font-cursive mb-4 text-gray-900">Meet Our Board of Directors</h3>
             <p class="text-base leading-relaxed mb-4">
+                Mr. Sushant Neupane, being a board of director in a company responsible for overseeing the management
+                and direction of a company as well as responsible for selecting and appointing the executive leadership
+                and acts as the governing body of a company.
+                <br />
                 Mr. Neupane is responsible for overseeing the activities, policies, and strategic direction of a company
                 and represents the interests of shareholders or members ensuring that the company is managed in a way
                 that aligns with its mission, goals, and legal obligations. He as a director plays a crucial role in the
@@ -45,7 +49,7 @@
                 with its stated goals and legal requirements.
                 However, as a company’s member, his main focus is to manufacture best quality of products, creating new
                 taste for our valued customers and is to build a company that can achieve the highest levels of customer
-                satisfaction with highest levels of integrity and business ethics. <br />
+                satisfaction with highest levels of integrity and business ethics.
             </p>
             <p class="text-xl font-bold text-gray-900">Sushant Neupane</p>
             <p class="text-base text-lime-500">BOD</p>
