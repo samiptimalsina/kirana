f7c6b4979978c8c4bc0f8359cc7036c9b54b9efa
Rishi Timalsina <rishi.timilsina@nexdigitalstudio.com>
2025-01-08 22:13:14 +0545

    changes on release

--- resources/views/home/partials/ceo-bod.blade.php
+++ resources/views/home/partials/ceo-bod.blade.php
@@ -20,7 +20,7 @@
     </div>
 </section>
 
-<section id="board-of-directors" class="max-w-screen-xl mx-auto p-8">
+{{-- <section id="board-of-directors" class="max-w-screen-xl mx-auto p-8">
     <div class="flex flex-wrap items-center justify-between gap-8">
         <div class="w-full lg:w-1/2 max-w-md">
             <img class="w-full h-auto rounded-lg shadow-lg" src="{{url('/')}}/teams/photo/1723140846.jpg" alt="Board of Directors Image" />
@@ -38,4 +38,95 @@
             <p class="text-base text-lime-500">BOD</p>
         </div>
     </div>
+</section> --}}
+
+
+
+
+
+{{-- <section id="board-of-directors" class="max-w-screen-xl mx-auto p-8">
+    <div class="flex flex-wrap items-center justify-between gap-8">
+        <div class="w-full lg:w-1/2 max-w-md">
+            <img class="w-full h-auto rounded-lg shadow-lg" src="{{ url('/') }}/teams/photo/sushant_neupane.jpeg"
+                alt="Board of Directors Image" />
+        </div>
+        <div class="w-full lg:w-1/2 text-gray-600">
+            <h3 class="text-2xl font-cursive mb-4 text-gray-900">Meet Our Board of Directors</h3>
+            <p class="text-base leading-relaxed mb-4">
+                Mr. Sushant Neupane, being a board of director in a company responsible for overseeing the management
+                and direction of a company as well as responsible for selecting and appointing the executive leadership
+                and acts as the governing body of a company. <br />
+                <br />
+                Mr. Neupane is responsible for overseeing the activities, policies, and strategic direction of a company
+                and represents the interests of shareholders or members ensuring that the company is managed in a way
+                that aligns with its mission, goals, and legal obligations. He as a director plays a crucial role in the
+                leadership and governance of a company, ensuring it operates effectively, ethically, and in alignment
+                with its stated goals and legal requirements.
+                However, as a company’s member, his main focus is to manufacture best quality of products, creating new
+                taste for our valued customers and is to build a company that can achieve the highest levels of customer
+                satisfaction with highest levels of integrity and business ethics.
+            </p>
+            <p class="text-xl font-bold text-gray-900">Sushant Neupane</p>
+            <p class="text-base text-lime-500">BOD</p>
+        </div>
+    </div>
+</section> --}}
+
+
+
+<section id="board-of-directors" class="max-w-screen-xl mx-auto p-8">
+    <div class="flex flex-wrap items-center justify-between gap-8">
+        <div class="w-full lg:w-1/2 max-w-md">
+            <img class="w-full h-auto rounded-lg shadow-lg" src="{{ url('/') }}/teams/photo/sushant_neupane.jpeg"
+                alt="Board of Directors Image" />
+        </div>
+        <div class="w-full lg:w-1/2 text-gray-600">
+            <h3 class="text-2xl font-cursive mb-4 text-gray-900">Meet Our Board of Directors</h3>
+            <p class="text-base leading-relaxed mb-4">
+                Mr. Sushant Neupane, being a board of director in a company responsible for overseeing the management
+                and direction of a company as well as responsible for selecting and appointing the executive leadership
+                and acts as the governing body of a company. <br />
+                <br />
+                Mr. Neupane is responsible for overseeing the activities, policies, and strategic direction of a company
+                and represents the interests of shareholders or members ensuring that the company is managed in a way
+                that aligns with its mission, goals, and legal obligations. He as a director plays a crucial role in the
+                leadership and governance of a company, ensuring it operates effectively, ethically, and in alignment
+                with its stated goals and legal requirements.
+                However, as a company’s member, his main focus is to manufacture best quality of products, creating new
+                taste for our valued customers and is to build a company that can achieve the highest levels of customer
+                satisfaction with highest levels of integrity and business ethics.
+            </p>
+            <p class="text-xl font-bold text-gray-900">Sushant Neupane</p>
+            <p class="text-base text-lime-500">BOD</p>
+        </div>
+    </div>
 </section>
+
+
+
+{{-- <section id="ceo-message" class="max-w-screen-xl mx-auto p-8">
+    <div class="flex flex-wrap items-center justify-between gap-8">
+        <div class="w-full lg:w-1/2 text-gray-600">
+            <h3 class="text-2xl font-cursive mb-4 text-gray-900">Meet Our Board of Directors</h3>
+            <p class="text-base leading-relaxed mb-4">
+                Mr. Sushant Neupane, being a board of director in a company responsible for overseeing the management
+                and direction of a company as well as responsible for selecting and appointing the executive leadership
+                and acts as the governing body of a company. <br />
+                <br />
+                Mr. Neupane is responsible for overseeing the activities, policies, and strategic direction of a company
+                and represents the interests of shareholders or members ensuring that the company is managed in a way
+                that aligns with its mission, goals, and legal obligations. He as a director plays a crucial role in the
+                leadership and governance of a company, ensuring it operates effectively, ethically, and in alignment
+                with its stated goals and legal requirements.
+                However, as a company’s member, his main focus is to manufacture best quality of products, creating new
+                taste for our valued customers and is to build a company that can achieve the highest levels of customer
+                satisfaction with highest levels of integrity and business ethics.
+            </p>
+            <p class="text-xl font-bold text-gray-900">Sushant Neupane</p>
+            <p class="text-base text-lime-500">BOD</p>
+        </div>
+        <div class="w-full lg:w-1/2 max-w-md">
+            <img class="w-full h-auto rounded-lg shadow-lg" src="{{ url('/') }}/teams/photo/sushant_neupane.jpeg"
+                alt="Board of Directors Image" />        </div>
+    </div>
+</section> --}}
